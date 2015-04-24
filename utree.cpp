61fe507b56c5108c51ced1d209e7eb89fd385920
SUDHARSAN CHAKRAVARTHI <sudharsanprc@users.noreply.github.com>
2015-04-24 13:59:11 -0500

    Create utree.cpp

---
+++ utree.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : utree.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int height(int n) {
+    return 0;
+}
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << height(n) << endl;
+    }
+}
+
